b217f8134ac0ca0d7ecf350346e80cddf881aee5
Shreshta Vinayak <shreshtavinayak.1994@gmail.com>
2020-09-20 01:50:06 -0700

    Added Twilio

---
+++ Twilio Video app/build/precache-manifest.1f0d6329d95ad0251213d0bcab3074e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95af05381ea568b734e18ab679828cc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a692dc906a79f3cbc790",
+    "url": "/static/css/main.64bc34eb.chunk.css"
+  },
+  {
+    "revision": "fe08cc7454a9fdd82814",
+    "url": "/static/js/2.0132d690.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0132d690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a692dc906a79f3cbc790",
+    "url": "/static/js/main.5de387d4.chunk.js"
+  },
+  {
+    "revision": "fcfe827e8c153e3bcda5",
+    "url": "/static/js/runtime-main.5ad136f7.js"
+  }
+]);
